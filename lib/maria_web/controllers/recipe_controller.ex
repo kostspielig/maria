04826e3303065d6aa847e71bcf7141e2398803bd
kostspielig <kostspielig@gmail.com>
2023-02-16 03:17:56 +0100

    Add basic cooking / recipes resource

---
+++ lib/maria_web/controllers/recipe_controller.ex
@@ -0,0 +1,62 @@
+defmodule MariaWeb.RecipeController do
+  use MariaWeb, :controller
+
+  alias Maria.Recipes
+  alias Maria.Recipes.Recipe
+
+  def index(conn, _params) do
+    recipes = Recipes.list_recipes()
+    render(conn, :index, recipes: recipes)
+  end
+
+  def new(conn, _params) do
+    changeset = Recipes.change_recipe(%Recipe{})
+    render(conn, :new, changeset: changeset)
+  end
+
+  def create(conn, %{"recipe" => recipe_params}) do
+    case Recipes.create_recipe(recipe_params) do
+      {:ok, recipe} ->
+        conn
+        |> put_flash(:info, "Recipe created successfully.")
+        |> redirect(to: ~p"/recipes/#{recipe}")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    recipe = Recipes.get_recipe!(id)
+    render(conn, :show, recipe: recipe)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    recipe = Recipes.get_recipe!(id)
+    changeset = Recipes.change_recipe(recipe)
+    render(conn, :edit, recipe: recipe, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "recipe" => recipe_params}) do
+    recipe = Recipes.get_recipe!(id)
+
+    case Recipes.update_recipe(recipe, recipe_params) do
+      {:ok, recipe} ->
+        conn
+        |> put_flash(:info, "Recipe updated successfully.")
+        |> redirect(to: ~p"/recipes/#{recipe}")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :edit, recipe: recipe, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    recipe = Recipes.get_recipe!(id)
+    {:ok, _recipe} = Recipes.delete_recipe(recipe)
+
+    conn
+    |> put_flash(:info, "Recipe deleted successfully.")
+    |> redirect(to: ~p"/recipes")
+  end
+end
---
+++ priv/repo/migrations/20230216010816_create_recipes.exs
@@ -0,0 +1,19 @@
+defmodule Maria.Repo.Migrations.CreateRecipes do
+  use Ecto.Migration
+
+  def change do
+    create table(:recipes) do
+      add :title, :string
+      add :descrition, :text
+      add :directions, :text
+      add :mins, :integer, default: 0, null: false
+      add :ingredients, {:array, :string}
+      add :likes, :integer, default: 0
+      add :cover, :string
+      add :link, :string
+      add :tags, {:array, :string}
+
+      timestamps()
+    end
+  end
+end

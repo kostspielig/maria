9757ad138391ffce66aef06a2f111465bc5b4507
kostspielig <kostspielig@gmail.com>
2023-02-27 14:35:44 +0100

    Upgrade due to issue in previous version
    https://github.com/phoenixframework/phoenix/issues/5102

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Maria.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.3"},
+      {:phoenix_live_view, "~> 0.18.16"},
       {:heroicons, "~> 0.5"},
       {:floki, ">= 0.30.0"}, #, only: :test},
       {:httpoison, "~> 2.0.0"},

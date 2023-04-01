fd92a5ecbce0f3df387777dae9d719d84b4b13b0
kostspielig <kostspielig@gmail.com>
2023-04-01 20:00:41 +0200

    Check for changes in etag value asyncronously.

--- lib/maria_web/cache.ex
+++ lib/maria_web/cache.ex
@@ -8,7 +8,6 @@ defmodule MariaWeb.Cache do
 
   @cache_name :http_cache
   @cache_ttl 12 * 60 * 60 * 1000  # 12 hours in milliseconds
-  # @cache_capacity 10_000          # maximum number of items in cache
 
 
    @doc """
@@ -26,14 +25,43 @@ defmodule MariaWeb.Cache do
         response = make_request(url)
         Cachex.put(@cache_name, url, response, ttl: @cache_ttl)
         {:ok, response}
-      {:ok, value} ->
+      {:ok, response} ->
+        %HTTPoison.Response{status_code: 200, headers: headers} = response
+        etag = getHeader(headers, "ETag")
+
+        if etag do
+          Task.start_link(fn ->
+            case make_request(url, [{"If-None-Match", etag}]) do
+              response = %HTTPoison.Response{status_code: 200, } ->
+                Logger.debug("Cache hit for #{url}, found updates")
+                Cachex.put(@cache_name, url, response)
+              _ ->
+                Logger.debug("Cache hit for #{url}, found NO updates")
+                :ok
+            end
+          end)
+        end
         Logger.debug("Cache hit for #{url}")
-        {:ok, value}
+        {:ok, response}
     end
   end
 
   defp make_request(url) do
     {:ok, response} = HTTPoison.get(url)
     response
+  end
+
+  defp make_request(url, params) do
+    {:ok, response} = HTTPoison.get(url, params)
+    response
+  end
+
+  defp getHeader(headers, header) do
+    h = Enum.find(headers, fn {x,_} -> x == header end)
+    if h do
+      elem(h, 1)
+    else
+      nil
+    end
   end
 end

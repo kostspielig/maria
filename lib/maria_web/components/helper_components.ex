b15d01d9acc2e1cecc7e3cdd3cf2c261f79e64b2
kostspielig <kostspielig@gmail.com>
2023-06-08 16:31:26 +0200

    Fix content showing over the header

--- lib/maria_web/components/helper_components.ex
+++ lib/maria_web/components/helper_components.ex
@@ -74,7 +74,7 @@ defmodule MariaWeb.HelperComponents do
 
   def page_header(assigns) do
     ~H"""
-    <header class="px-4 py-6 sm:px-6 lg:px-8 xl:px-28 bg-white sticky top-0">
+    <header class="px-4 py-6 sm:px-6 lg:px-8 xl:px-28 bg-white sticky top-0 z-10">
       <div class="mx-auto max-w-xl lg:mx-auto">
         <a  href="/"  class="flex md:justify-center text-4xl font-racing font-semibold text-brand bg-white uppercase">
           carras.co

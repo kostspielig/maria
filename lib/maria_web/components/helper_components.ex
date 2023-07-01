4a89dfbc9ce20ae059cba19a812f04480402b773
kostspielig <kostspielig@gmail.com>
2023-07-01 23:11:09 +0200

    Add new cool links 🤘

--- lib/maria_web/components/helper_components.ex
+++ lib/maria_web/components/helper_components.ex
@@ -56,6 +56,20 @@ defmodule MariaWeb.HelperComponents do
     """
   end
 
+  attr :href, :string, required: true
+  attr :class, :string, default: nil
+
+  slot :inner_block, required: true
+  def link_hover(assigns) do
+    ~H"""
+    <a class={["group transition-all duration-300 ease-in-out", @class]} href={"#{@href}"}>
+      <span class="bg-left-bottom bg-gradient-to-r from-blood to-blood bg-[length:0%_12px] bg-no-repeat group-hover:bg-[length:100%_12px] transition-all duration-500 ease-out">
+        <%= render_slot(@inner_block) %>
+      </span>
+    </a>
+    """
+  end
+
 
   defp extract_host(url) do
     case URI.parse(url).host do

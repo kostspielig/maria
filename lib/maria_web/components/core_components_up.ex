a74c05709e5dc550bfdaf77335b268e3a23d536f
kostspielig <kostspielig@gmail.com>
2023-07-02 11:11:31 +0200

    Better form display in mobile + formatting

--- lib/maria_web/components/core_components_up.ex
+++ lib/maria_web/components/core_components_up.ex
@@ -66,7 +66,7 @@ defmodule MariaWeb.CoreComponentsUp do
               phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
               phx-key="escape"
               phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
-              class="shadow-zinc-700/10 ring-zinc-700/10 relative hidden rounded-2xl bg-white p-14 shadow-lg ring-1 transition"
+              class="shadow-zinc-700/10 ring-zinc-700/10 relative hidden rounded-2xl bg-white py-8 px-6 sm:py-14 sm:px-14 shadow-lg ring-1 transition"
             >
               <div class="absolute top-6 right-5">
                 <button

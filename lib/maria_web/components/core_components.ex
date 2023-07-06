2a248ebbb3c506acf0effc99ac59ce95ab663f67
kostspielig <kostspielig@gmail.com>
2023-07-06 21:41:52 +0200

    Fix modal (on top of header)

--- lib/maria_web/components/core_components.ex
+++ lib/maria_web/components/core_components.ex
@@ -51,7 +51,7 @@ defmodule MariaWeb.CoreComponents do
       id={@id}
       phx-mounted={@show && show_modal(@id)}
       phx-remove={hide_modal(@id)}
-      class="relative z-50 hidden"
+      class="relative z-[80] hidden"
     >
       <div id={"#{@id}-bg"} class="fixed inset-0 bg-zinc-50/90 transition-opacity" aria-hidden="true" />
       <div
--- lib/maria_web/components/core_components_up.ex
+++ lib/maria_web/components/core_components_up.ex
@@ -48,7 +48,7 @@ defmodule MariaWeb.CoreComponentsUp do
       phx-mounted={@show && show_modal(@id)}
       phx-remove={hide_modal(@id)}
       data-cancel={JS.exec(@on_cancel, "phx-remove")}
-      class="relative z-50 hidden"
+      class="relative z-[80] hidden"
     >
       <div id={"#{@id}-bg"} class="bg-zinc-50/90 fixed inset-0 transition-opacity" aria-hidden="true" />
       <div

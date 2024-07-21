3369004204c361740f50a8024648fb2e78877555
kostspielig <kostspielig@gmail.com>
2024-07-21 19:56:48 +0200

    Fix language and test environment

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@ pkgs.mkShell {
   buildInputs = [
     beam.interpreters.erlang_25 # Erlang interpreter
     beam.packages.erlang_25.elixir_1_14 # Elixir 1.14
-    pkgs.nodejs-18_x # Node.js, you can specify the version you need
+    pkgs.nodejs-18_x # Node.js
     pkgs.inotify-tools # inotify-tools for file watching
     pkgs.wget # wget for fetching dependencies
     pkgs.postgresql # PostgreSQL client tools
@@ -20,6 +20,10 @@ pkgs.mkShell {
 
     # Ensure that PostgreSQL binaries are available in the shell
     export PATH=${pkgs.postgresql}/bin:$PATH
+
+    # Ensure UTF-8 locale is used
+    export LANG=en_US.UTF-8
+    export LC_CTYPE=en_US.UTF-8
 
     echo "Environment setup for Elixir 1.14 and Phoenix development"
   '';
